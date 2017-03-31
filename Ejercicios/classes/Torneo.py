6a2713c19fe3e5ef1fe4342d262d2798e63c2cb8
jgros <jgros2000@gmail.com>
2017-03-31 11:34:51 -0300

    dfs

--- Ejercicios/classes/Torneo.py
+++ Ejercicios/classes/Torneo.py
@@ -1,3 +1,6 @@
+from .Partido import Partido
+
+
 class Torneo(object):
     listaEquipos=[]
     listaPartidos=[]
@@ -10,3 +13,31 @@ class Torneo(object):
         self.listaEquipos.append(e)
 
     def organizarFixture(self):
+        self.encontrarHorario()
+
+    def encontrarCoincidencia(self, item1,dia, turno):
+        for item in self.listaEquipos:
+            if item.disponibilidad[dia][turno] == True and item.nombre!=item1.nombre:
+                return item, True
+
+    def encontrarHorario(self):
+        for item in self.listaEquipos:
+            for item2 in range(6):
+                for item3 in range(3):
+                    if item.disponibilidad[item2][item3] == True:
+                        equipo, coincide=self.encontrarCoincidencia(item,item2,item3)
+                        if coincide == True:
+                            self.crearPartido(item,equipo,item2,item3)
+
+    def crearPartido(self,equipo1,equipo2,dia,turno):
+        partido = Partido()
+        partido.Dia=dia
+        partido.Turno=turno
+        partido.Equipo1=equipo1
+        partido.Equipo2=equipo2
+        self.listaPartidos.append(partido)
+
+
+
+
+
